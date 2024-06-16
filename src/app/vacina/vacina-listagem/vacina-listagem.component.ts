6a942fca277f581796232d35925943b23c82b501
André Sampaio <sam_ger@hotmail.com>
2024-06-16 14:20:26 -0300

    CONSTRUÇÃO DA TELA PRINCIPAL DE CONSULTA DE VACINAS COM FILTROS

--- src/app/vacina/vacina-listagem/vacina-listagem.component.ts
+++ src/app/vacina/vacina-listagem/vacina-listagem.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Vacina } from '../../shared/model/vacina';
 import { VacinaSeletor } from '../../shared/model/seletor/vacina.seletor';
 import { VacinaDTO } from '../../shared/model/dto/vacina.DTO';
-import { VacinaService } from '../../shared/service/vacina.service';
 import Swal from 'sweetalert2';
-import { FabricanteService } from '../../shared/service/fabricante.service';
-import { Fabricante } from '../../shared/model/fabricante';
-import { UnidadeService } from '../../shared/service/unidade.service';
-import { Unidade } from '../../shared/model/unidade';
 import { EstoqueService } from '../../shared/service/estoque.service';
 
 @Component({
@@ -19,9 +13,11 @@ import { EstoqueService } from '../../shared/service/estoque.service';
 export class VacinaListagemComponent implements OnInit{
 
     public vacinaSeletor: VacinaSeletor = new VacinaSeletor();
-
     public listaVacinasDTO : Array<VacinaDTO> = new Array();
-
+    public listaDeVacinas : Array<string> = new Array();
+    public listaDeCategorias : Array<string> = new Array();
+    public listaDeFabricantes : Array<string> = new Array();
+    public listaDeUnidades : Array<string> = new Array();
 
     constructor(
       private estoqueService : EstoqueService,
@@ -51,12 +47,84 @@ export class VacinaListagemComponent implements OnInit{
     this.estoqueService.consultarComFiltros(this.vacinaSeletor).subscribe(
       (resultado) => {
         this.listaVacinasDTO = resultado;
-
+        this.construirListaDeCategorias();
+        this.construirListaDeVacinas();
+        this.construirListaDeFabricantes();
+        this.construirListaDeUnidades();
       },
       (erro) => {
         Swal.fire('Erro ao consultar a lista de vacinas com o(s) filtro(s) selecionado(s). ','','error');
       }
     );
+  }
+
+  public construirListaDeVacinas(): void{
+    this.listaDeVacinas = [];
+    for(let vDTO of this.listaVacinasDTO){
+      let vacinaExiste = false;
+      for(let vacina of this.listaDeVacinas){
+        if(vDTO.vacina.nome.trim().toLowerCase() == vacina.trim().toLowerCase()){
+          vacinaExiste = true;
+          break;
+        }
+      }
+      if(!vacinaExiste){
+        this.listaDeVacinas.push(vDTO.vacina.nome);
+      }
+    }
+    this.listaDeVacinas.sort();
+  }
+
+
+  public construirListaDeCategorias(): void{
+    this.listaDeCategorias = [];
+    for(let vDTO of this.listaVacinasDTO){
+      let categoriaExiste = false;
+      for(let categoria of this.listaDeCategorias){
+        if(vDTO.vacina.categoria.trim().toLowerCase() == categoria.trim().toLowerCase()){
+          categoriaExiste = true;
+          break;
+        }
+      }
+      if(!categoriaExiste){
+        this.listaDeCategorias.push(vDTO.vacina.categoria);
+      }
+    }
+    this.listaDeCategorias.sort();
+  }
+
+  public construirListaDeFabricantes(): void{
+    this.listaDeFabricantes = [];
+    for(let vDTO of this.listaVacinasDTO){
+      let fabricanteExiste = false;
+      for(let fabricante of this.listaDeFabricantes){
+        if(vDTO.fabricante.nome.trim().toLowerCase() == fabricante.trim().toLowerCase()){
+          fabricanteExiste = true;
+          break;
+        }
+      }
+      if(!fabricanteExiste){
+        this.listaDeFabricantes.push(vDTO.fabricante.nome);
+      }
+    }
+    this.listaDeFabricantes.sort();
+  }
+
+  public construirListaDeUnidades(): void{
+    this.listaDeUnidades = [];
+    for(let vDTO of this.listaVacinasDTO){
+      let unidadeExiste = false;
+      for(let unidade of this.listaDeUnidades){
+        if(vDTO.unidade.nome.trim().toLowerCase() == unidade.trim().toLowerCase()){
+          unidadeExiste = true;
+          break;
+        }
+      }
+      if(!unidadeExiste){
+        this.listaDeUnidades.push(vDTO.unidade.nome);
+      }
+    }
+    this.listaDeUnidades.sort();
   }
 }
 
