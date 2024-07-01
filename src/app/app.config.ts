63c3565adca7220a1cddec416a0c1dcdecac8aa3
André Sampaio <sam_ger@hotmail.com>
2024-07-01 16:38:05 -0300

    mais ajustes de configurações iniciais

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
+import { requestAngular17Interceptor } from './auth/request-angular17.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch()),
-    provideHttpClient(withInterceptors([intercept])),
+    // provideHttpClient(withFetch()), //  provideHttpClient(withFetch()) será ainda necessária?
+    provideHttpClient(withInterceptors([requestAngular17Interceptor]))
   ]
 };
--- src/app/shared/components/rodape/rodape.component.ts
+++ src/app/shared/components/rodape/rodape.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rodape', //Esse atributo que define o nome que será usado na tag para referenciar o componente
                           //<app-rodape></app-rodape>
+  standalone: true, // explicação no final
   templateUrl: './rodape.component.html',
   styleUrls: ['./rodape.component.scss']
 })
@@ -14,4 +15,22 @@ export class RodapeComponent implements OnInit {
   }
 
 }
+
+/*
+
+Standalone: true: Torna o componente autônomo, permitindo importá-lo diretamente em outros componentes sem precisar de um módulo.
+
+Sem standalone: true: O componente precisa estar declarado em um módulo (como AppModule) e ser importado via esse módulo. Não pode ser importado diretamente por outros componentes.
+
+"Resumo"
+
+Com standalone: true: O RodapeComponent é autônomo e pode ser importado diretamente em qualquer outro componente.
+Sem standalone: true: O RodapeComponent precisa ser declarado em um módulo (RodapeModule), e esse módulo deve ser importado onde o RodapeComponent será usado.
+
+
+"Explicação mais detalhada:"
+
+https://chatgpt.com/c/7eb7dec0-6e21-4607-b8dc-df4f44b6e8a9
+
+*/
 
